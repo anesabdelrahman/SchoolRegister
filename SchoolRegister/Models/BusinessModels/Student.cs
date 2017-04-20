75a1fb5ca11bd0eb11a9de9360b0a4e4f312f81f
Anes <anes.abdelrahman@gmail.com>
2017-04-20 23:31:14 +0100

    Adding custom display names to properties.

--- SchoolRegister/Models/BusinessModels/Student.cs
+++ SchoolRegister/Models/BusinessModels/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using SchoolRegister.Common;
 
@@ -11,22 +12,33 @@ namespace SchoolRegister.Models.BusinessModels
         public int StudentId { get; set; }
 
         [Required]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [DisplayName("Surname")]
         public string LastName { get; set; }
+
+        [DisplayName("Middle Name")]
         public string MiddleName { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
+        [DisplayName("Date of Birth")]
         public DateTime DateOfBirth { get; set; }
+
+        [DisplayName("Received Books")]
         public bool ReceivedBooks { get; set; }
 
+        [DisplayName("English School")]
         public string EnglishSchool { get; set; }
+
+        [DisplayName("Medical Information")]
         public string MedicalInfo { get; set; }
         public Gender Gender { get; set; }
 
         [Required]
+        [DisplayName("Can Be Photograph")]
         public bool CanBePhotographed { get; set; }
         public ICollection<Parent> Parents { get; set; }
         public Address Address { get; set; }
